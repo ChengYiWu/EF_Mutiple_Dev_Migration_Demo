12eacd8984eb3a1335a4fae4fd4a54f834812636
chris_cy_wu <chris_cy_wu@gss.com.tw>
2018-07-16 11:32:41 +0800

    加入專案檔案。

---
+++ EF_Mutiple_Dev_Migration_Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Mutiple_Dev_Migration_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
