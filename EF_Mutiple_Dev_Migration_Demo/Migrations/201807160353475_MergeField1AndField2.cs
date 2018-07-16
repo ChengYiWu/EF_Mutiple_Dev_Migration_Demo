b10f4fa177d3892eded0302d7a4a5538ac49a24f
chris_cy_wu <chris_cy_wu@gss.com.tw>
2018-07-16 11:55:48 +0800

    1. 加入空的 Migration，並 IgnoreChange，使得目前 Entity Model 狀態與 EF Migration 的 snapshot 一致，就可以解決 EF 判定 Entity Model 和 Migration 不一致，導致程式不能運行的問題

---
+++ EF_Mutiple_Dev_Migration_Demo/Migrations/201807160353475_MergeField1AndField2.cs
@@ -0,0 +1,16 @@
+namespace EF_Mutiple_Dev_Migration_Demo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MergeField1AndField2 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
